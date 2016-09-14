6261686d647fb4e3d5f56c5ee343ba54533221b6
Tim <tmc1618@gmail.com>
2016-09-14 09:04:27 -0500

    fixed form width for event name

--- event/forms.py
+++ event/forms.py
@@ -8,6 +8,7 @@ class EventForm(forms.ModelForm):
         widgets = {
             'date': forms.DateInput(attrs={'class': 'datepicker'}),
             'time': forms.TimeInput(attrs={'class': 'timepicker'}),
+            
         }
         fields = ('organization', 'name', 'type', 'date', 'time', 'cash', 'admin_fee')
 
